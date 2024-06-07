612902804f45f1aefce3660ef5e97246651d42a9
Hayden Amazon <167032968+haydenamazon@users.noreply.github.com>
2024-06-07 16:39:25 -0400

    Add files via upload

---
+++ UserGeneration.py
@@ -0,0 +1,83 @@
+import csv
+
+with open('testcsv.csv', 'r', encoding='utf-8-sig') as file:
+    csv_reader = csv.reader(file)
+    data = []
+    for row in csv_reader:
+        data.append(row)
+#print(data)     
+usernames = []
+for d in data:
+    temp = d[0][0].lower() + d[1].lower()
+    usernames.append(temp)
+#print(usernames)
+
+onedarray = [item for sublist in data for item in sublist]
+#print(onedarray)
+
+len_array1 = len(onedarray)
+len_array2 = len(usernames)
+testarray = []
+
+for i in range(0, len_array1, 2):
+    testarray.extend(onedarray[i:i+2])
+    if usernames:
+        testarray.append(usernames.pop(0))
+testarray.extend(usernames)
+  
+powershelltemplate = """
+$Attributes = @firstbracket
+    Enabled = $true
+    ChangePasswordAtLogon = $true
+
+    UserPrincipalName = "{UserPrincipalName}"
+    Name = "{Name}"
+    GivenName = "{GivenName}"
+    Surname = "{Surname}"
+    DisplayName = "{DisplayName}"
+    Description = "This is an account."
+    Office = "None."
+
+    Company = "finalproject.com"
+    Department = "IT"
+    Title = "IT Master"
+    City = "Macon"
+    State = "Georgia"
+
+    AccountPassword = "p@ssword@1" | ConvertTo-SecureString -AsPlainText -Force
+secondbracket
+
+New-ADUser @Attributes
+
+net localgroup Administrators /add finalproject\{Name}
+"""
+
+MasterSc = []
+x = 1
+while len(testarray) != 0:    
+    Gname = testarray[0]
+    testarray.pop(0)
+    Sname = testarray[0]
+    testarray.pop(0)
+    Fname = testarray[0]
+    testarray.pop(0)
+    email = Fname + "@finalproject.com"
+    Dname = Gname + " " + Sname
+    #print(Gname + " " + Sname + " " + Fname + " " + email + " " + Dname)
+    powershell = powershelltemplate.format(
+        UserPrincipalName = email,
+        Name = Fname,
+        GivenName = Gname,
+        Surname = Sname,
+        DisplayName = Dname
+    )
+    finPS = powershell.replace("firstbracket", "{").replace("secondbracket", "}")
+    MasterSc.append(finPS)
+    #print(finPS)
+
+script = "".join(MasterSc)
+#print(script)
+f = open("UserScript.ps1", "w")
+f.write(script)
+f.close()
+print("User Script Created.")
